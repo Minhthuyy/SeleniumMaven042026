02490b0f2c6faf0496ce9b4d9bc95a05bb47f15f
Minhthuyy <minhthuyy16@gmail.com>
2026-04-09 17:16:40 +0700

    update

---
+++ src/test/java/com/anhtester/Bai5_Locators/LocatorsHTML.java
@@ -0,0 +1,9 @@
+package com.anhtester.Bai5_Locators;
+
+public class LocatorsHTML {
+//    public static void main(String[] args) throws InterruptedException {
+//
+//        driver.get()
+//
+//    }
+}
